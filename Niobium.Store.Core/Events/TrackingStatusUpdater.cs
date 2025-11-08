d58e42145b788bbf14e7eb19b82df96a7df40951
Wen Han <hcp5he11@gmail.com>
2025-11-08 22:36:07 +1300

    Bugfix.

--- Niobium.Store.Core/Events/TrackingStatusUpdater.cs
+++ Niobium.Store.Core/Events/TrackingStatusUpdater.cs
@@ -4,6 +4,8 @@ namespace Niobium.Store.Events
 {
     internal class TrackingStatusUpdater(IDomainRepository<OrderDomain, Order> repo) : DomainEventHandler<IDomain<Order>, UpdateTrackingCommand>
     {
+        protected override DomainEventAudience EventSource => DomainEventAudience.External;
+
         public override async Task HandleCoreAsync(UpdateTrackingCommand e, CancellationToken cancellationToken = default)
         {
             var domain = await repo.GetAsync(Order.BuildPartitionKey(e.Customer), Order.BuildRowKey(e.Order), cancellationToken: cancellationToken);
